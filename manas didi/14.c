60f715f5b9368dcaf44bb21d0d9994b9f8fda538
saurabhmaurya45 <tgtsaurabhmaurya45@gmail.com>
2022-01-23 00:52:56 +0530

    update in 14.c

--- manas didi/14.c
+++ manas didi/14.c
@@ -3,9 +3,31 @@
 #include <stdio.h>
 
 int main(){
+    int n,temp,arr[100],digit_count=0;
+    printf("Enter number to check whether it is palindrome or not: ");
+    scanf("%d",&n);
+    temp=n;
+    
+    while(n>0){
+        arr[digit_count]=n%10;
+        digit_count++;
+        n=n/10;
+    }
 
+    int flag=1;
+    for(int i=0; i<digit_count/2;i++){
+        if(arr[i]!=arr[digit_count-i-1]){
+            flag=0;
+            break;
+        }
+    }
 
-
+    if(flag){
+        printf("%d is palindrome",temp);
+    }
+    else{
+        printf("%d is not palindrome",temp);
+    }
 
     return 0;
 }
--- manas didi/70.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-
-    for (char i =0; i<1000; i++){
-        printf("%d\n",i);
-    }
-    return 0;
-}
